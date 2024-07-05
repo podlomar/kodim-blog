679ff286f41664b60d33dd2bc6562b2847a07d23
Martin Podloucký <podlouckymartin@gmail.com>
2024-07-05 12:28:26 +0200

    Opravy překlepů a doplnění

--- articles/hexkody/dev/process.mjs
+++ articles/hexkody/dev/process.mjs
@@ -5,32 +5,32 @@ const filepath = '/home/podlomar/crazy-16.bmp';
 const buffer = await fs.readFile(filepath);
 const bytes = Array.from(buffer);
 
-// let line = '  0: ';
-// for (let i = 0; i < bytes.length; i++) {
-//   const binary = bytes[i].toString(2).padStart(8, '0');
-//   line += binary;
-  
-//   if (i % 8 === 7) {
-//     console.log(line);
-//     line = `${i.toString().padStart(3, ' ')}: `;
-//   } else {
-//     line += ' ';
-//   }
-// }
-
-// console.log(line);
-
-let line = '0000: ';
+let line = '  0: ';
 for (let i = 0; i < bytes.length; i++) {
-  const hex = bytes[i].toString(16).padStart(2, '0');
-  line += hex;
+  const binary = bytes[i].toString(2).padStart(8, '0');
+  line += binary;
   
-  if (i % 16 === 15) {
+  if (i % 8 === 7) {
     console.log(line);
-    line = `${i.toString(16).padStart(4, '0')}: `;
+    line = `${(i+1).toString().padStart(3, ' ')}: `;
   } else {
     line += ' ';
   }
 }
 
 console.log(line);
+
+// let line = '0000: ';
+// for (let i = 0; i < bytes.length; i++) {
+//   const hex = bytes[i].toString(16).padStart(2, '0');
+//   line += hex;
+  
+//   if (i % 16 === 15) {
+//     console.log(line);
+//     line = `${(i + 1).toString(16).padStart(4, '0')}: `;
+//   } else {
+//     line += ' ';
+//   }
+// }
+
+// console.log(line);
